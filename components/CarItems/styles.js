464081a6a2c596d32ba1447133f40f67681cb8e2
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-11 23:21:31 +0530

    created styles for car item

---
+++ components/CarItems/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  carContainer: {
+    width: "100 %",
+    height: "100 %",
+  },
+  titleContainer: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  modelName: {
+    fontSize: 40,
+    fontWeight: 600,
+  },
+  modelDescription: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+});
+
+export default styles;
