48b10ddd1df172770013045735da9a8c38081687
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-12 23:31:12 +0530

    changes

--- components/cars.js
+++ components/cars.js
@@ -1,41 +1,35 @@
 export default [
   {
-    id: 1,
     modelName: "Model S",
     modelImage: require("../assets/images/ModelS.jpeg"),
     tagline: "order online for",
     tagSub: "TouchLess Delivery ",
   },
   {
-    id: 2,
     modelName: "Model 3",
     modelImage: require("../assets/images/Model3.jpeg"),
     tagline: "order online for",
     tagSub: "TouchLess Delivery ",
   },
   {
-    id: 3,
     modelName: "Model X",
     modelImage: require("../assets/images/ModelX.jpeg"),
     tagline: "order online for",
     tagSub: "TouchLess Delivery ",
   },
   {
-    id: 4,
     modelName: "Model Y",
     modelImage: require("../assets/images/ModelY.jpeg"),
     tagline: "order online for",
     tagSub: "TouchLess Delivery ",
   },
   {
-    id: 5,
     modelName: "Solar Panel",
     modelImage: require("../assets/images/SolarPanels.jpeg"),
     tagline: "order online for",
     tagSub: "TouchLess Delivery ",
   },
   {
-    id: 6,
     modelName: "Solar Roof",
     modelImage: require("../assets/images/SolarRoof.jpeg"),
     tagline: "order online for",
