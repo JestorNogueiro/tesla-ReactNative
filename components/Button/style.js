e19725f9a0cf65a4ad24b1502e176ac8ab6fc6d2
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-12 16:36:26 +0530

    button styles

---
+++ components/Button/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    padding: 8,
+  },
+  button: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: "500",
+    textTransform: "uppercase",
+  },
+});
+
+export default styles;
