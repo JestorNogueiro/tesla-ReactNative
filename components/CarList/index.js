b2dfecd572ee10207742b8f4a5f63da5e0e8f4c0
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-12 16:38:11 +0530

    componet to render cars

---
+++ components/CarList/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import styles from "./style";
+import cars from "../cars";
+import CarItem from "../CarItems";
+const CarList = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList data={cars} renderItem={(item) => <CarItem car={item} />} />
+    </View>
+  );
+};
+
+export default CarList;
