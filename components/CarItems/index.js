c2fbb3866b8afc35304ce01536482354efb94f56
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-12 16:37:10 +0530

    component to render car list

--- components/CarItems/index.js
+++ components/CarItems/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
+import Button from "../Button";
 import styles from "./styles";
-const CarItem = () => {
+const CarItem = (props) => {
+  const { modelName, modelDesc, modelImage, tagline, tagSub } = props.car;
+  console.log(props.car);
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/images/ModelX.jpeg")}
-        style={styles.backgroundImage}
-      />
+      <ImageBackground source={modelImage} style={styles.backgroundImage} />
       <View style={styles.titleContainer}>
-        <Text style={styles.modelName}>Model S</Text>
-        <Text style={styles.modelDescription}>Starting at $69,333</Text>
+        <Text style={styles.modelName}>{modelName}</Text>
+        <Text style={styles.modelDescription}>
+          {tagline} <Text style={styles.tagSub}>{tagSub}</Text>
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          type="primary"
+          content="custom order"
+          onPress={console.warn("you been warned")}
+        />
+        <Button
+          type="secondary"
+          content="existing inventory"
+          onPress={console.warn("you been warned")}
+        />
       </View>
     </View>
   );
